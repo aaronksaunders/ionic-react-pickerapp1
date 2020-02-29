e3412cf4227922f3c4bf59b9019f9fac04388613
Aaron Saunders <aaronksaunders@gmail.com>
2020-02-29 17:49:55 -0500

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,81 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonItem,
+  IonLabel
+} from "@ionic/react";
+import React, { useState } from "react";
+import "./Home.css";
+import MyPicker from "../components/MyPicker";
+
+export interface ISessionTime {
+  weekday: string;
+  period: string;
+}
 
 const Home: React.FC = () => {
+  const [pickerIsOpen, setPickerIsOpen] = useState(false);
+  const [sessionTime, setSessionTime] = useState<ISessionTime | undefined>(
+    undefined
+  );
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Ionic React</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+        <IonHeader>
+          <IonToolbar title="condense">
+            <IonLabel>Picker Sample</IonLabel>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonItem>
+          <IonButton
+            onClick={() => {
+              setPickerIsOpen(true);
+            }}
+          >
+            Select Session
+          </IonButton>
+          <IonButton
+            onClick={() => {
+              setSessionTime(undefined);
+            }}
+          >
+            Clear Session
+          </IonButton>
+        </IonItem>
+        <IonItem
+          onClick={() => {
+            setPickerIsOpen(true);
+          }}
+        >
+          {sessionTime ? (
+            <IonLabel>
+              {sessionTime?.weekday} - {sessionTime?.period}
+            </IonLabel>
+          ) : (
+            <IonLabel className="placeHolder">Please Select Session</IonLabel>
+          )}
+        </IonItem>
+        <MyPicker
+          isOpen={pickerIsOpen}
+          onCancel={() => {
+            setPickerIsOpen(false);
+          }}
+          onSave={(_value: any) => {
+            console.log(_value);
+            let { Day, SessionTime } = _value;
+            setSessionTime({ weekday: Day.value, period: SessionTime.value });
+            setPickerIsOpen(false);
+          }}
+        />
       </IonContent>
     </IonPage>
   );
